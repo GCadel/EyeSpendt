73d5da1483fde97647b17264f0dec4948f0a7a0c
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-02-06 15:53:30 -0700

    Configure DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./database/connect");
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Hello from Spendt!" });
 });
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(port, async () => {
+  try {
+    await connectDB(process.env.MONGO);
+    console.log(`App listening on port ${port}`);
+  } catch (error) {
+    console.log("Error connecting to DB:", error.message);
+  }
 });
