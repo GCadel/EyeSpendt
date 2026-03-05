ba4d0ca93e8295c7fe63e8b6257e4e62e7a4df22
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-03-05 01:05:50 -0500

    Removed unused console log

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,7 +12,6 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
   if (!email || !password) {
     throw new BadRequestError("Email and password required");
