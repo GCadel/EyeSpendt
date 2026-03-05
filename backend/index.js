8866fc9bbcd4bc8f17a76cc96246d019437eb396
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-03-05 15:40:13 -0500

    Added routers to app

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const connectDB = require("./database/connect");
 const TransactionRouter = require("./routes/Transaction.router");
+const AuthRouter = require("./routes/auth.router");
+const AuthMiddleware = require("./middleware/authentication");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/transactions", TransactionRouter);
+// Routes
+app.use("/api/transactions", AuthMiddleware, TransactionRouter);
+app.use("/api/auth", AuthRouter);
 
 app.get("/", (req, res) => {
-  console.log(req.body);
   res.status(200).json({ msg: "Hello from Spendt!" });
 });
+
+// Error handling middleware
+app.use(require("./middleware/error-handler"));
+
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    // Attempt to connect to MongoDB
     await connectDB(process.env.MONGO);
+    // Listen for connection
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
     });
