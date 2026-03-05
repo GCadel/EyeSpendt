4ebc0b2f342800101cb854d3f87afcb8dfbebbbb
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-03-05 15:36:53 -0500

    Added auth to transaction API

--- backend/controllers/Transaction.controller.js
+++ backend/controllers/Transaction.controller.js
@@ -1,18 +1,22 @@
+const { NotFound } = require("../errors");
 const Transaction = require("../models/Transaction");
 const { StatusCodes } = require("http-status-codes");
 
 const getTransactions = async (req, res) => {
-  const transactions = await Transaction.find().sort("transactionDate");
+  const transactions = await Transaction.find({
+    createdBy: req.user.id,
+  }).sort("transactionDate");
   return res.status(StatusCodes.OK).json({ transactions });
 };
 
 const getTransaction = async (req, res) => {
   const { id: _id } = req.params;
-  const transaction = await Transaction.findOne({ _id });
+  const transaction = await Transaction.findOne({
+    _id,
+    createdBy: req.user.id,
+  });
   if (!transaction) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ msg: "Transaction not found" });
+    throw new NotFound("Transaction not found");
   }
   return res.status(StatusCodes.OK).json({ transaction });
 };
@@ -21,27 +25,30 @@ const updateTransaction = async (req, res) => {
   const {
     params: { id: _id },
     body,
+    user: { id: userId },
   } = req;
 
   const transaction = await Transaction.findOneAndUpdate(
     {
       _id,
+      createdBy: userId,
     },
     body,
     { runValidators: true, new: true },
   );
 
   if (!transaction) {
-    return res
-      .status(StatusCodes.IM_A_TEAPOT)
-      .json({ msg: "Error updating transaction" });
+    throw new NotFound("Transaction does not exist");
   }
 
   return res.status(StatusCodes.ACCEPTED).json({ transaction });
 };
 
 const createTransaction = async (req, res) => {
-  const transaction = await Transaction.create(req.body);
+  const transaction = await Transaction.create({
+    ...req.body,
+    createdBy: req.user.id,
+  });
   return res.status(StatusCodes.CREATED).json({ transaction });
 };
 
@@ -49,12 +56,11 @@ const deleteTransaction = async (req, res) => {
   const { id: _id } = req.params;
   const transaction = await Transaction.findOneAndDelete({
     _id,
+    createdBy: req.user.id,
   });
 
   if (!transaction) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ msg: "No transaction found" });
+    throw new NotFound("Could not find transaction");
   }
 
   return res
