eb30be35cdc885e571cdb8fe0b3bb626ab03ae6e
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-03-05 01:02:49 -0500

    Create auth API

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, Unauthenticated } = require("../errors");
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: { name: user.getFullName(), monthlyBudget: user.getMonthlyBudget() },
+    token,
+  });
+};
+
+const login = async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Email and password required");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Unauthenticated("Incorrect email or password");
+  }
+  const passIsCorrect = await user.checkPassword(password);
+  if (!passIsCorrect) {
+    throw new Unauthenticated("Incorrect email or password");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.getFullName(),
+      monthlyBudget: user.getMonthlyBudget(),
+    },
+    token,
+  });
+};
+
+module.exports = { register, login };
