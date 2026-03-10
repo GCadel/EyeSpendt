d435c943767c814608088ffcb73a59ede5d80a7e
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-03-10 00:19:24 -0400

    Fix date off-by-1 bug

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -2,6 +2,11 @@ import { Link } from "react-router";
 
 export const TransactionItem = ({ data, reloadHandler }) => {
   const { amount, category, transactionDate, description, _id: id } = data;
+  const formatDate = (aDate) => {
+    let formattedDate = String(aDate).split("T")[0];
+    let [year, month, day] = formattedDate.split("-");
+    return `${month}/${day}/${year}`;
+  };
   return (
     <>
       <div>
@@ -12,7 +17,7 @@ export const TransactionItem = ({ data, reloadHandler }) => {
       </div>
       <div>
         <div>{category}</div>
-        <div>{new Date(transactionDate).toLocaleDateString()}</div>
+        <div>{formatDate(transactionDate)}</div>
       </div>
       <button onClick={() => reloadHandler(id)}>🗑️ Delete</button>
       <Link to={`/editTransaction/${id}`}>📝 Edit</Link>
